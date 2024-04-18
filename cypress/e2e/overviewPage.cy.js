53e0c66b5f5362df057260d6b9b909b8a68dd28d
Natalia Maternia <nat.maternia@gmail.com>
2024-04-18 15:06:28 +0200

    add assertion to overview Page tests

--- cypress/e2e/overviewPage.cy.js
+++ cypress/e2e/overviewPage.cy.js
@@ -5,11 +5,13 @@ import MainPage from "../pageObjects/mainPage";
 import CartPage from "../pageObjects/cartPage";
 import CheckoutPage from "../pageObjects/checkoutPage";
 import OverviewPage from "../pageObjects/overviewPage";
+import CompletePage from "../pageObjects/completePage";
 const loginPage = new LoginPage();
 const mainPage = new MainPage();
 const cartPage = new CartPage();
 const checkoutPage = new CheckoutPage();
 const overviewPage = new OverviewPage();
+const completePage = new CompletePage();
 
 describe("Checkout tests", () => {
   beforeEach(() => {
@@ -18,18 +20,31 @@ describe("Checkout tests", () => {
     mainPage.clickOnshoppingcart();
     cartPage.clickOnCheckoutButton();
     checkoutPage.fillUserInformation();
-    checkoutPage.clickOnContinueButton();
   });
 
   it("click on cancel button", () => {
+    // arrange
+    checkoutPage.clickOnContinueButton();
+    // act
     overviewPage.clickOnCancelButton();
+    // assert
+    mainPage.verifyIfProductElementIsVisible();
+
   });
 
   it("click on finish button", () => {
+    // arrange
+    checkoutPage.clickOnContinueButton();
+    // act
     overviewPage.clickOnFinishButton();
+    // assert
+    completePage.verifyIfOrderIsComplete();
   });
 
   it("check pathname of checkout:'overview' page", () => {
+    // act
+    checkoutPage.clickOnContinueButton();
+    // assert
     overviewPage.pathnameOverwiewPage();
   });
 
